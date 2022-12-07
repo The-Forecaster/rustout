498e0db7fa24c90e21f9f09774f58a53f57d092c
The-Forecaster <DarkDruid11@gmail.com>
2022-12-07 11:05:40 -0700

    Started rendering stuff

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@
 // Since: 12/5/22
 // Breakout in rust
 
+use piston_window::{PistonWindow, WindowSettings, clear, rectangle};
+
 fn main() {
-    println!("Test")
+    let mut app: PistonWindow = WindowSettings::new("Breakout", [500, 500]).exit_on_esc(true).build().unwrap();
+
+    while let Some(event) = &mut app.next() {
+        app.draw_2d(event, |context, graphics, _device| {
+            clear([1.0; 4], graphics);
+
+            rectangle([1.0, 0.0, 0.0, 1.0], [0.0, 0.0, 100.0, 100.0], context.transform, graphics);
+        });
+    }
 }
